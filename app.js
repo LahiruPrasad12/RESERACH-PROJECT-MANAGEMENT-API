c3f35394e2a6cf6881643fba3da6be927cf1d5be
LahiruPrasad12 <lahirupr471@gmail.com>
2023-10-03 00:25:13 +0530

    changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const csp = require('helmet-csp');
 
 const AppError = require("./Utils/appError");
 
@@ -39,11 +40,23 @@ const corsOptions = {
   credentials: true, // Enable passing cookies and other credentials
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 //GLOBAL MIDDLEWARES
 
 // Set security HTTP headers
 app.use(helmet());
+
+app.use(
+  csp({
+    directives: {
+      'default-src': ["'self'"],
+      'script-src': ["'self'", 'cdnjs.cloudflare.com'],
+      'style-src': ["'self'", 'fonts.googleapis.com'],
+      'img-src': ["'self'", 'data:', 'cdn.example.com'],
+    },
+  })
+);
+
 
 // Development logging
 // if (process.env.NODE_ENV === "development") {
@@ -51,35 +64,35 @@ app.use(helmet());
 // }
 
 // Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
-});
-app.use("/api", limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many requests from this IP, please try again in an hour!",
+// });
+// app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
+// app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss());
+// app.use(xss());
 
 // Prevent parameter pollution
-app.use(
-  hpp({
-    whitelist: [
-      "duration",
-      "ratingsQuantity",
-      "ratingsAverage",
-      "maxGroupSize",
-      "difficulty",
-      "price",
-    ],
-  })
-);
+// app.use(
+//   hpp({
+//     whitelist: [
+//       "duration",
+//       "ratingsQuantity",
+//       "ratingsAverage",
+//       "maxGroupSize",
+//       "difficulty",
+//       "price",
+//     ],
+//   })
+// );
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
@@ -100,9 +113,9 @@ app.use("/api/message", messageRoutes);
 app.use("/api/download", fileDownload);
 app.use("/api/document", document);
 
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+// app.all("*", (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
 
 // app.use(globalErrorHandler);
 
