1010cb6fefd14e95d87d504469f2fa51855b3e01
LahiruPrasad12 <lahirupr471@gmail.com>
2023-10-03 00:37:27 +0530

    changes

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const corsOptions = {
   credentials: true, // Enable passing cookies and other credentials
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 //GLOBAL MIDDLEWARES
 
 // Set security HTTP headers
@@ -59,40 +59,40 @@ app.use(
 
 
 // Development logging
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Limit requests from same API
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: "Too many requests from this IP, please try again in an hour!",
-// });
-// app.use("/api", limiter);
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-// app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
-// app.use(mongoSanitize());
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
-// app.use(xss());
+app.use(xss());
 
 // Prevent parameter pollution
-// app.use(
-//   hpp({
-//     whitelist: [
-//       "duration",
-//       "ratingsQuantity",
-//       "ratingsAverage",
-//       "maxGroupSize",
-//       "difficulty",
-//       "price",
-//     ],
-//   })
-// );
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
@@ -113,10 +113,10 @@ app.use("/api/message", messageRoutes);
 app.use("/api/download", fileDownload);
 app.use("/api/document", document);
 
-// app.all("*", (req, res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// });
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 module.exports = app;
